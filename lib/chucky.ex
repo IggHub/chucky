0e53742451e3672326926e01525f6317e75763e5
Iggy <igoririanto@rocketmail.com>
2018-05-05 21:00:34 -0500

    adding application behavior - TBC

--- lib/chucky.ex
+++ lib/chucky.ex
@@ -1,18 +1,27 @@
 defmodule Chucky do
-  @moduledoc """
-  Documentation for Chucky.
-  """
+  use Application
+  require Logger
 
-  @doc """
-  Hello world.
+  def start(type, _args) do
+    import Supervisor.Spec
+    children = [
+      worker(Chucky.Server, [])
+    ]
+    case type do
+      :normal ->
+        Logger.info("Application is started on #{node}")
+      {:takeover, old_node} ->
+        Logger.info("#{node} is taking over #{old_node}")
+      {:failover, old_node} ->
+        Logger.info("#{old_node} is failing over to #{node}")
+    end
 
-  ## Examples
-
-      iex> Chucky.hello
-      :world
-
-  """
-  def hello do
-    :world
+    opts = [strategy: :one_for_one, name: {:global, Chucky.Supervisor}]
+    Supervisor.start_link(children, opts)
   end
+
+  def fact() do
+    Chucky.Server.fact
+  end
+ 
 end
